66ac26226036798cd6fbf6871865a81ec375f84e
Valtteri Pyyhtiä <valtteri.pyyhtia@helsinki.fi>
2018-01-27 14:44:07 +0200

    Using objects instead of separate variables

--- src/index.js
+++ src/index.js
@@ -10,9 +10,9 @@ const Otsikko = (props) => {
 const Sisalto = (props) => {
     return (
         <div>
-            <Osa nimi={props.osa1} tehtavienLukumaara={props.tehtavia1} />
-            <Osa nimi={props.osa2} tehtavienLukumaara={props.tehtavia2} />
-            <Osa nimi={props.osa3} tehtavienLukumaara={props.tehtavia3} />
+            <Osa nimi={props.osa1.nimi} tehtavienLukumaara={props.osa1.tehtavia} />
+            <Osa nimi={props.osa2.nimi} tehtavienLukumaara={props.osa2.tehtavia} />
+            <Osa nimi={props.osa3.nimi} tehtavienLukumaara={props.osa3.tehtavia} />
         </div>
     )
 }
@@ -31,18 +31,24 @@ const Yhteensa = (props) => {
 
 const App = () => {
     const kurssi = 'Half Stack -sovelluskehitys'
-    const osa1 = 'Reactin perusteet'
-    const tehtavia1 = 10
-    const osa2 = 'Tiedonvälitys propseilla'
-    const tehtavia2 = 7
-    const osa3 = 'Komponenttien tila'
-    const tehtavia3 = 14
+    const osa1 = {
+        nimi: 'Reactin perusteet',
+        tehtavia: 10
+    }
+    const osa2 = {
+        nimi: 'Tiedonvälitys propseilla',
+        tehtavia: 7
+    }
+    const osa3 = {
+        nimi: 'Komponenttien tila',
+        tehtavia: 14
+    }
 
     return (
         <div>
             <Otsikko kurssi={kurssi} />
-            <Sisalto osa1={osa1} osa2={osa2} osa3={osa3} tehtavia1={tehtavia1} tehtavia2={tehtavia2} tehtavia3={tehtavia3} />
-            <Yhteensa yhteensa={tehtavia1 + tehtavia2 + tehtavia3} />
+            <Sisalto osa1={osa1} osa2={osa2} osa3={osa3} />
+            <Yhteensa yhteensa={osa1.tehtavia + osa2.tehtavia + osa3.tehtavia} />
         </div>
     )
 }
