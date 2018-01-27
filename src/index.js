0707e44ee7d95160d4f0a0ced08355c14e0fae2a
Valtteri Pyyhtiä <valtteri.pyyhtia@helsinki.fi>
2018-01-27 14:35:14 +0200

    Split to components

--- src/index.js
+++ src/index.js
@@ -1,6 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+const Otsikko = (props) => {
+    return (
+        <h1>{props.kurssi}</h1>
+    )
+}
+
+const Sisalto = (props) => {
+    return (
+        <p>{props.nimi} {props.tehtavienLukumaara}</p>
+    )
+}
+
+const Yhteensa = (props) => {
+    return (
+        <p>yhteensä {props.yhteensa} tehtävää</p>
+    )
+}
+
 const App = () => {
     const kurssi = 'Half Stack -sovelluskehitys'
     const osa1 = 'Reactin perusteet'
@@ -12,11 +30,11 @@ const App = () => {
 
     return (
         <div>
-            <h1>{kurssi}</h1>
-            <p>{osa1} {tehtavia1}</p>
-            <p>{osa2} {tehtavia2}</p>
-            <p>{osa3} {tehtavia3}</p>
-            <p>yhteensä {tehtavia1 + tehtavia2 + tehtavia3} tehtävää</p>
+            <Otsikko kurssi={kurssi} />
+            <Sisalto nimi={osa1} tehtavienLukumaara={tehtavia1} />
+            <Sisalto nimi={osa2} tehtavienLukumaara={tehtavia2} />
+            <Sisalto nimi={osa3} tehtavienLukumaara={tehtavia3} />
+            <Yhteensa yhteensa={tehtavia1 + tehtavia2 + tehtavia3} />
         </div>
     )
 }
