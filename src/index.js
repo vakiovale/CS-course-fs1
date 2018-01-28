640e45867ae433cb9773ba03ac6e67cf8f90353d
Valtteri Pyyhtiä <valtteri.pyyhtia@gmail.com>
2018-01-28 14:25:12 +0200

    Added random anecdotes

--- src/index.js
+++ src/index.js
@@ -1,110 +1,49 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const Statistics = ({ palaute }) => {
-    const eiPalautetta = palaute.hyva + palaute.neutraali + palaute.huono === 0;
-
-    if(eiPalautetta) {
-        return (
-            <div>
-                <h2>Statistiikka</h2>
-                <p>Ei palautetta</p>
-            </div>
-        )
-    } else {
-        return (
-            <div>
-                <h2>Statistiikka</h2>
-                <table>
-                    <tbody>
-                        <Yhteenveto nimi="Hyvä" palaute={palaute.hyva} />
-                        <Yhteenveto nimi="Neutraali" palaute={palaute.neutraali} />
-                        <Yhteenveto nimi="Huono" palaute={palaute.huono} />
-                        <PalauteKeskiarvo palaute={palaute} />
-                        <Positiivinen palaute={palaute} />
-                    </tbody>
-                </table>
-            </div>
-        )
-    }
-}
-
-const Statistic = ({ nimi, arvo}) => {
-    return (
-        <tr>
-            <td>{nimi}</td>
-            <td>{arvo}</td>
-        </tr>
-    )
-}
-
-const Yhteenveto = ({ nimi, palaute }) => {
-    return (
-        <Statistic nimi={nimi} arvo={palaute} />
-    )
-}
-
-const PalauteKeskiarvo = ({ palaute }) => {
-    const keskiarvo = ((palaute.hyva * 1) + (palaute.huono*(-1))) 
-    / (palaute.hyva + palaute.neutraali + palaute.huono);
-    
-    return (
-        <Statistic nimi="keskiarvo" arvo={Number(keskiarvo).toFixed(2)} />
-    )
-}
-
-const Positiivinen = ({ palaute }) => {
-    const positiivisia = (palaute.hyva) 
-    / (palaute.hyva + palaute.neutraali + palaute.huono);
-
-    const arvo = Number(100 * positiivisia).toFixed(1) + " %";
-
-    return (
-        <Statistic nimi="positiivisia" arvo={arvo} />
-    )
-}
-
-const Button = ({handleClick, text}) => {
-    return (
-        <button onClick={handleClick}>
-            {text}
-        </button> 
-    )
-}
-
 class App extends React.Component {
     constructor(props) {
         super(props)
-        this.state = {
-            hyva: 0,
-            neutraali: 0,
-            huono: 0
+            this.state = {
+            selected: 0
         }
     }
 
-    annaPalautetta = (palaute) => {
+    randomAnecdote = () => {
         return () => {
-            this.setState({ [palaute]: this.state[palaute] + 1 });
-            console.log(this.state[palaute]);
+            const randomAnecdoteIndex = Math.floor(Math.random() * anecdotes.length);
+            console.log("Random anecdote: ", (randomAnecdoteIndex + 1));
+            this.setState({ selected: randomAnecdoteIndex });
         }
     }
 
     render() {
         return (
             <div>
-                <div>
-                    <h2>Anna palautetta</h2>
-                    <Button handleClick={this.annaPalautetta('hyva')} text="Hyvä" />
-                    <Button handleClick={this.annaPalautetta('neutraali')} text="Neutraali" />
-                    <Button handleClick={this.annaPalautetta('huono')} text="Huono" />
-                </div>
-                <Statistics palaute={this.state} />
+                <p>{this.props.anecdotes[this.state.selected]}</p>
+                <Button handleClick={this.randomAnecdote()} text="Next anecdote" />
             </div>
         )
     }
 }
 
+
+const Button = ({ handleClick, text }) => {
+    return (
+        <button onClick={handleClick}>{text}</button>
+    )
+}
+
+const anecdotes = [
+    'If it hurts, do it more often',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+]
+
 ReactDOM.render(
-    <App />, 
+    <App anecdotes={anecdotes} />,
     document.getElementById('root')
 )
